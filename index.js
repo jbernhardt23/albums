39968ba2773c4142793a8366d67664438df8d3df
Jose Bernhardt <jose.bn23@gmail.com>
2018-12-27 23:12:19 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+/** @format */
+
+import React from 'react';
+import { Text, AppRegistry } from 'react-native';
+
+const App = () => (
+        <Text>Some textttt</Text>
+    );
+
+AppRegistry.registerComponent('albums', () => App);
